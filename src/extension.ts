cff79cbeada5bed3fe8974752228881cea4845dc
Sten <62704405+ghostkiller967@users.noreply.github.com>
2022-05-12 20:42:41 +0200

    Add files via upload

--- src/extension.ts
+++ src/extension.ts
@@ -1,48 +1,57 @@
-import vscode from "vscode";
+import vscode from 'vscode';
 import _open from "open";
 import fs from "fs";
 import fetch from "node-fetch";
+import http from "https";
 
-async function download(url: string, dest: string): Promise<void> {
-	const response = await fetch(url);
-	const fileStream = fs.createWriteStream(dest);
-	await new Promise((resolve, reject) => {
-		response.body!.pipe(fileStream);
-		response.body!.on("error", reject);
-		fileStream.on("finish", resolve);
-	});
+interface Version {
+	version: string;
 }
 
-export function activate(context: vscode.ExtensionContext): void {
-
-	if (!fs.existsSync("Pipe.exe")) {
-		download("https://api.github.com/repos/ghostkiller967/VSEcutor/contents/Pipe.exe", "Pipe.exe");
-	}
+export function activate(context: vscode.ExtensionContext) {
 
 	context.subscriptions.push(vscode.commands.registerCommand("vsecutor.inject", () => {
 		_open("Pipe.exe", {
 			app: {
 				name: "Pipe.exe",
-				arguments: [ "-Attach", vscode.workspace.getConfiguration().get("conf.settingsEditor.dllPath") as string, vscode.workspace.getConfiguration().get("conf.settingsEditor.targetProcess") as string ],
+				arguments: ["-Attach", vscode.workspace.getConfiguration().get("conf.settingsEditor.dllPath") as string, vscode.workspace.getConfiguration().get("conf.settingsEditor.targetProcess") as string],
 			},
 		});
 	}));
 
 	context.subscriptions.push(vscode.commands.registerCommand("vsecutor.execute", (uri: vscode.Uri | undefined) => {
 		const filePath = uri?.fsPath ?? vscode.window.activeTextEditor?.document.uri.fsPath;
-		if (!filePath) { 
+		if (!filePath) {
 			vscode.window.showErrorMessage("You need to have a file open to use this command, this has to be a saved file on the drive.");
 			return;
 		}
 		_open("Pipe.exe", {
 			app: {
 				name: "Pipe.exe",
-				arguments: [ "-Execute", vscode.workspace.getConfiguration().get("conf.settingsEditor.pipe") as string, filePath ],
+				arguments: ["-Execute", vscode.workspace.getConfiguration().get("conf.settingsEditor.pipe") as string, filePath],
 			},
 		});
 	}));
+
+	context.subscriptions.push(vscode.commands.registerCommand("vsecutor.update", async () => {
+		const response = await fetch("https://raw.githubusercontent.com/ghostkiller967/VSEcutor/main/PipeVersion.json");
+		const latestVersion: Version = JSON.parse(await response.text());
+		if (fs.existsSync("PipeVersion.json")) {
+			const currentVersion: Version = JSON.parse(fs.readFileSync("PipeVersion.json", "utf8"));
+			if (currentVersion.version !== latestVersion.version) {
+				fs.writeFileSync("PipeVersion.json", JSON.stringify({ "version": latestVersion.version }));
+				const file = fs.createWriteStream("Pipe.exe");
+				http.get("https://raw.githubusercontent.com/ghostkiller967/VSEcutor/main/Pipe.exe", function(response) {
+					response.pipe(file);
+				});
+			}
+		} else {
+			fs.writeFileSync("PipeVersion.json", JSON.stringify({ "version": latestVersion.version }));
+			http.get("https://raw.githubusercontent.com/ghostkiller967/VSEcutor/main/Pipe.exe", function(response) {
+				response.pipe(fs.createWriteStream("Pipe.exe"));
+			});
+		}
+	}));
 }
- 
-export function deactivate(): void {
-	
-}
+
+export function deactivate() {}
